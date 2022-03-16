ac6c8814f9e347c7450cb3c97f9af9ee04994ab5
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2022-03-16 15:36:34 -0400

    Added new component

--- EventRegistration-Frontend/src/router/index.js
+++ EventRegistration-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import EventRegistration from '@/components/EventRegistration'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/app',
+      name: 'EventRegistration',
+      component: EventRegistration
     }
   ]
 })
